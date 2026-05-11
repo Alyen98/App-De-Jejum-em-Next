701f36b09843b416f62f863d9282aa4b11e793a3
ALYSON FREITAS DOS SANTOS <freitas.alyson02@gmail.com>
2026-05-11 20:04:45 -0300

    feat: configuração do firebase auth e variáveis de ambiente
    Signed-off-by: ALYSON FREITAS DOS SANTOS <freitas.alyson02@gmail.com>

---
+++ sistema-calorias-jejum/src/app/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA2IM8QgDcEczUcv30eLJE_uMb07wd_zwo",
+    authDomain: "sistema-calorias-jejum.firebaseapp.com",
+    projectId: "sistema-calorias-jejum",
+    storageBucket: "sistema-calorias-jejum.firebasestorage.app",
+    messagingSenderId: "78378433224",
+    appId: "1:78378433224:web:9c95dd59ed51bab30343fd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
